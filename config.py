1fac5b00ceddec3d7faf114408fa49d06830e8ee
Dmitry Granovsky <dima.granovsky@gmail.com>
2024-04-14 22:26:28 +0300

    fix default host

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from constants import LANG_RU
 TOKEN = "<your token here>"
 LOG_NAME = "main.log"
 
-DEFAULT_HOST = "http://158.160.14.10:8080"
+DEFAULT_HOST = "158.160.14.10:8080"
 
 DEFAULT_DELAY = 5  # minutes
 UPDATE_FREQUENCY = 15  # seconds
